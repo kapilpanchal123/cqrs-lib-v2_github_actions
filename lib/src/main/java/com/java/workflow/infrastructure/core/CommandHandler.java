33d2d0eaba72eb3ed2b5c39605d47a4b95d17556
kapil-panchal <80364696+kapil-panchal@users.noreply.github.com>
2025-12-27 13:17:06 +0530

    [cqrs] type token added

--- lib/src/main/java/com/java/workflow/infrastructure/core/CommandHandler.java
+++ lib/src/main/java/com/java/workflow/infrastructure/core/CommandHandler.java
@@ -3,10 +3,13 @@ package com.java.workflow.infrastructure.core;
 import com.google.common.reflect.TypeToken;
 
 public interface CommandHandler<REQ,RES> {
+
+  TypeToken<REQ> requestTypeToken();
+
   RES handle(Command<REQ> command);
 
   default boolean matches(Command<?> command) {
-    TypeToken<REQ> handlerType = new TypeToken<REQ>() {};
-    return handlerType.getRawType().isAssignableFrom(command.getPayload().getClass());
+//    TypeToken<REQ> handlerType = new TypeToken<>() {};
+    return requestTypeToken().getRawType().isAssignableFrom(command.getPayload().getClass());
   }
 }
