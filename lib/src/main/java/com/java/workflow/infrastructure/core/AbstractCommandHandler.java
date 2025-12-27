b4655a8d6635d5018d201684a63714143442cff7
kapil-panchal <80364696+kapil-panchal@users.noreply.github.com>
2025-12-27 17:26:00 +0530

    [handler] added abstract command handler

---
+++ lib/src/main/java/com/java/workflow/infrastructure/core/AbstractCommandHandler.java
@@ -0,0 +1,17 @@
+package com.java.workflow.infrastructure.core;
+
+import com.google.common.reflect.TypeToken;
+
+public abstract class AbstractCommandHandler<REQ,RES> implements CommandHandler<REQ,RES> {
+
+  private final TypeToken<REQ> requestTypeToken;
+
+  protected AbstractCommandHandler() {
+    this.requestTypeToken = new TypeToken<REQ>(getClass()) {};
+  }
+
+  @Override
+  public TypeToken<REQ> requestTypeToken() {
+    return requestTypeToken;
+  }
+}
